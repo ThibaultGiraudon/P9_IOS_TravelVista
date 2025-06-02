f784d4a4ad70e56b84ed2cc12f38dfbce88bbb4e
ThibaultGiraudon <thibault.giraudon@gmail.com>
2025-06-02 15:59:32 +0200

    Add ListView in appDelegate

--- TravelVista/TravelVista/AppDelegate.swift
+++ TravelVista/TravelVista/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftUI
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let hostingController = UIHostingController(rootView: ListView())
+        self.window?.rootViewController = hostingController
         return true
     }
 }
